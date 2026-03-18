b7999a5599cd948edfb3cf98555276970d7f920c
Muhammad faisal <muhammadfaisal7864309@gmail.com>
2026-03-18 14:21:22 +0500

    Register api and login api setup

--- src/app.js
+++ src/app.js
@@ -1,4 +1,7 @@
 const express = require("express");
 const app = express();
 app.use(express.json());
+
+const authRoutes = require("./routes/auth.routes");
+app.use("/api/auth", authRoutes);
 module.exports = app;
---
+++ src/routes/auth.routes.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const authController = require("../controllers/auth.controller");
+const authRouter = express.Router();
+
+// this is just for routing purpose, the actual logic will be in the controller
+//api/auth/register
+authRouter.post("/register", authController.registerController);
+//api/auth/login
+authRouter.post("/login", authController.loginController);
+
+module.exports = authRouter;
